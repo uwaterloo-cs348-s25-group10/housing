62c0418f548f42e986972ef1bc39f13699519b57
Minjae Lee <minjaelee0727@gmail.com>
2025-07-24 03:08:53 -0400

    feat: add import data button on client
    This is required to fix import data error on make file

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
 import canadaImage from "../assets/homepage-thumbnail.png";
 import "./Home.css";
+import { apiClient } from "../config/api";
 
 const Home = () => {
+  const handleImportData = async () => {
+    try {
+      await apiClient.post('/import-data/')
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return (
     <main className="home-container">
       <div className="home-text">
@@ -16,7 +24,7 @@ const Home = () => {
           Use historical data to explore housing trends, compare income levels,
           and find affordable regions tailored to you.
         </p>
-        <button className="explore-button">Start Exploring</button>
+        <button className="explore-button" onClick={handleImportData}>Import Data</button>
         <a href="/data-gaps" style={{ textDecoration: "none" }}>
           <button className="explore-button" style={{ marginLeft: "1rem" }}>Find Data Gaps</button>
         </a>
